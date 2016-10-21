3a055391b4e5733404b6ad52ed347973e1db8cf9
asherkory <asher.kory@gmail.com>
2016-10-21 10:59:05 -0400

    Add season team membership

---
+++ lib/sportscaster/resources/season_team_membership.rb
@@ -0,0 +1,10 @@
+module Sportscaster
+  class SeasonTeamMembership < Base
+    field :id
+    field :season_team_id
+    field :player_id
+    field :created_at
+    field :updated_at
+    
+  end
+end

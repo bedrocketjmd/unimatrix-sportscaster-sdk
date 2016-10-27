6dae5085c3de180f3deafb297079e191c0abbdc7
asherkory <asher.kory@gmail.com>
2016-10-27 13:48:35 -0400

    Change membership to player on game goalie and player statistics

--- lib/sportscaster/resources/game_goalie_statistic.rb
+++ lib/sportscaster/resources/game_goalie_statistic.rb
@@ -5,7 +5,7 @@ module Sportscaster
     field :provider
     field :created_at
     field :updated_at
-    field :game_team_membership_id
+    field :game_team_player_id
     field :saves
     field :shots_against
     field :goals_against

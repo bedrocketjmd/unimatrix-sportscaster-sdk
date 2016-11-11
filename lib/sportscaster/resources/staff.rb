de156793acb2e7d7ade971872b48204ff1f069dc
asherkory <asher.kory@gmail.com>
2016-11-11 12:32:48 -0500

    Add Staff resource

---
+++ lib/sportscaster/resources/staff.rb
@@ -0,0 +1,14 @@
+module Sportscaster
+  class Staff < Base
+    field :id
+    field :uuid
+    field :provider
+    field :created_at
+    field :updated_at
+    field :season_team_id
+    field :first_name
+    field :last_name
+    field :type_name
+
+  end
+end

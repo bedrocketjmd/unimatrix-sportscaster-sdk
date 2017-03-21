5f1ded5011e33175c773f734fbeafbad8407bd11
David Bragdon <davidbragdon@bedrocket.com>
2017-03-21 15:01:59 -0400

    SR-673 Replace season_team_id with team_id in Sportscaster::StaffMember

--- lib/sportscaster/resources/staff_member.rb
+++ lib/sportscaster/resources/staff_member.rb
@@ -5,7 +5,7 @@ module Sportscaster
     field :provider
     field :created_at
     field :updated_at
-    field :season_team_id
+    field :team_id
     field :first_name
     field :last_name
     field :type_name
